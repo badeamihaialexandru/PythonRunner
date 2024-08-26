c5df2028657c1410993ebdafd4bda724f88fb2db
Mihai Badea <badeamihaialexandru@gmail.com>
2024-08-26 20:11:56 +0300

    fix

--- python_executer.py
+++ python_executer.py
@@ -1,10 +1,18 @@
 import os
+import sys 
+from io import StringIO
 
 class PythonExecutor():
 
     def execute_code(self,code):
         self.save_in_txt(code)
-        return exec(code)
+        print(f"running the following code: {code}")
+        old_stdout = sys.stdout
+        redirected_output = sys.stdout = StringIO()
+        exec(code)
+        sys.stdout = old_stdout
+        print(redirected_output.getvalue())
+        return redirected_output.getvalue()
     
     def save_in_txt(self,code):
         if os.path.exists('/history.txt'):
